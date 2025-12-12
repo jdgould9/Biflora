e2a984e7a98da8ced9fe0914e4e7680cc7c177cd
jdgould9 <jdgould9@gmail.com>
2025-12-12 13:12:13 -0500

    initialization

---
+++ spring-garden/spring-garden/src/main/java/net/jdgould/spring_garden/SpringGardenApplication.java
@@ -0,0 +1,13 @@
+package net.jdgould.spring_garden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGardenApplication.class, args);
+	}
+
+}
---
+++ spring-garden/spring-garden/src/test/java/net/jdgould/spring_garden/SpringGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jdgould.spring_garden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
